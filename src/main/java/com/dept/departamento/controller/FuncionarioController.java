f9e7812b8041dddee718a9d86952a8a8463a0608
thur <arthur_ferrari@estudante.sesisenai.org.br>
2025-10-20 14:46:28 -0300

    .

---
+++ src/main/java/com/dept/departamento/controller/FuncionarioController.java
@@ -0,0 +1,67 @@
+package com.psii.appfiliacao.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.psii.appfiliacao.model.Filiacao;
+import com.psii.appfiliacao.service.FiliacaoService;
+import com.psii.appfiliacao.service.MaeService;
+import com.psii.appfiliacao.service.PaiService;
+
+@Controller
+@RequestMapping("/filiacoes")
+public class FiliacaoController {
+
+    @Autowired
+    private FiliacaoService filiacaoservice;
+
+    @Autowired
+    private MaeService maeService;
+
+    @Autowired
+    private PaiService paiService;
+
+    @GetMapping 
+    public String listarFiliacao(Model model) {
+        List<Filiacao> filiacao = filiacaoservice.listarFiliacao();
+        model.addAttribute("filiacao", filiacao);
+        return "lista_filiacoes";
+    }
+
+    @GetMapping("/novo")
+    public String novaFiliacao(Model model) {
+        model.addAttribute("filiacao", new Filiacao());
+        model.addAttribute("mae", maeService.listarMae());
+        model.addAttribute("pai", paiService.listarPai());
+        return "form_filiacao";
+    }
+
+    @PostMapping 
+    public String salvarFiliacao(@ModelAttribute Filiacao filiacao) {
+        filiacaoservice.salvarFiliacao(filiacao);
+        return "redirect:/filiacoes";
+    }
+
+    @GetMapping("/{id}/editar")
+    public String editarFiliacao(@PathVariable Long id, Model model) {
+        Filiacao filiacao = filiacaoservice.buscarFiliacao(id);
+        model.addAttribute("filiacao", filiacao);
+        model.addAttribute("mae", maeService.listarMae());
+        model.addAttribute("pai", paiService.listarPai());
+        return "form_filiacao";
+    }
+
+    @GetMapping("/{id}/deletar")
+    public String deletarConsulta(@PathVariable Long id){
+        filiacaoservice.deletarFiliacao(id);
+        return "redirect:/filiacoes";
+}
+}
---
+++ src/main/java/com/dept/departamento/controller/ProjetoController.java
@@ -0,0 +1,56 @@
+package com.psii.appfiliacao.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.psii.appfiliacao.model.Pai;
+import com.psii.appfiliacao.service.PaiService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequestMapping("/pais")
+public class PaiController {
+
+    @Autowired
+    private PaiService paiService;
+
+    @GetMapping
+    public String listarPai(Model model) {
+        List<Pai> pai = paiService.listarPai();
+        model.addAttribute("pai", pai);
+        return "lista_pais";
+    }
+
+    @GetMapping("/novo")
+    public String novoPai(Model model) {
+        model.addAttribute("pai", new Pai());
+        return "form_pai";
+    }
+
+    @PostMapping
+public String salvarPai(@ModelAttribute Pai pai) {
+    paiService.salvarPai(pai);
+    return "redirect:/pais";
+}
+
+    @GetMapping("/{id}/editar")
+    public String editarPai(@PathVariable Long id, Model model) {
+        Pai pai = paiService.buscarPai(id);
+        model.addAttribute("pai", pai);
+        return "form_pai";
+    }
+
+    @GetMapping("/{id}/deletar")
+    public String deletarPai(@PathVariable Long id) {
+        paiService.deletarPai(id);
+        return "redirect:/pais";
+    }
+}
