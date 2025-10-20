581114ac368558317446c3fb0f5c6f41d8ba0688
thur <arthur_ferrari@estudante.sesisenai.org.br>
2025-10-20 14:49:32 -0300

    .,.......

---
+++ src/main/java/com/dept/departamento/repository/ProjetoRepository.java
@@ -0,0 +1,9 @@
+package com.psii.appfiliacao.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.psii.appfiliacao.model.Pai;
+
+public interface PaiRepository extends JpaRepository<Pai, Long>{
+
+}
